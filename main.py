66370e4d923b6a69e06b519f00e02bd4a144d193
Rakesh Bhugra <81642485+rakeshbhugra@users.noreply.github.com>
2025-12-16 11:55:51 +0530

    augmentation, generation done, added mock db

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ Retrieval
 Augmented
 Generation
 
+R -> A -> G
+
+generator : user query, augmented prompt
+
 '''
 from litellm import completion
 import os
@@ -16,22 +20,33 @@ load_dotenv()
 def retrieve():
     pass
 
-def augment():
-    pass
+def augment(retrieved_docs, query):
+    user_prompt = f"<context> augmented prompt: {retrieved_docs} </context> based on the above context, answer the user query: {query}"
+    return user_prompt
 
-def generate():
-    pass
+def generate(user_prompt):
+    messages = [
+        {"role": "system", "content": "You are a helpful RAG assistant."},
+        {"role": "user", "content": user_prompt}
+    ]
 
+    response = completion(
+        model="openai/@openrouter-09cb28/openai/gpt-4.1-mini",
+        messages=messages,
+        api_key=os.getenv("PORTKEY_API_KEY"),
+        api_base=os.getenv("PORTKEY_API_BASE"),
+    )
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-]
+    return response.choices[0].message['content']
 
-response = completion(
-    model="openai/@openrouter-09cb28/openai/gpt-4.1-mini",
-    messages=messages,
-    api_key=os.getenv("PORTKEY_API_KEY"),
-    api_base=os.getenv("PORTKEY_API_BASE"),
-)
+    
+knowledge_base = {
+    "What is RAG?": "RAG stands for Retrieval-Augmented Generation, a technique that combines information retrieval with generative models to provide more accurate and context-aware responses.",
+    "How does RAG work?": "RAG works by first retrieving relevant documents or information based on a user's query, then augmenting the query with this information, and finally generating a response using a language model.",
+    "Who wrote '1984'?": "'1984' was written by George Orwell.",
+    "What is the capital of France?": "The capital of France is Paris.",
+}
 
-print(response)
+query = "What is RAG?"
+
+print(knowledge_base[query])
